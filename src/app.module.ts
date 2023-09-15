95c19b9fd563ac81add71fcd50d7867b75422379
ashish <ashish@keyvalue.systems>
2023-09-15 09:49:43 +0530

    Making intial changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required().min(10),
+      }),
+    }),
+    DatabaseModule,
+  ],
   providers: [],
 })
 export class AppModule {}
