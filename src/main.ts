02d9203ddf2b666b872e52ce93163cabc186b8be
ashish <ashish@keyvalue.systems>
2023-09-15 09:46:06 +0530

    Cleanup and adding database module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('talent-trail/api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  await app.listen(configService.get('PORT') || 4000);
 }
 bootstrap();
